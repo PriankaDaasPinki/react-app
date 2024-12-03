318d2a43d8cb0716c3a2fafe43d48fb522095b0a
PriankaDaasPink <priankadas55555@gmail.com>
2024-12-03 11:31:22 +0600

    back

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-// import logo from "./logo.svg";
+import React from "react";
+
+import "./CSS/navBar_SideBar.css";
 import "./CSS/App.css";
 import "./CSS/style.css";
-import React from "react";
-// import Navbar from "./components/Nav_Bar/NavBar.jsx";
-import "./CSS/navBar_SideBar.css";
 import RoutesAll from "./Routes.jsx";
-// import Navbar2 from "./components/Nav_Bar/NavBar2.jsx";
-// import Footer from "./components/common_components/Footer.jsx";
-// import Sidebar2 from "./components/Side_Bar/SideBar2.jsx";
 
 const App = () => {
   return (
